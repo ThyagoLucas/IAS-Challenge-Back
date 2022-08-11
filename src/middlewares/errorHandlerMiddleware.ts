359dfde4863010494169d24bab54d92fdc3a5c04
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-08-11 03:59:07 -0400

    feat:conclusion

--- src/middlewares/errorHandlerMiddleware.ts
+++ src/middlewares/errorHandlerMiddleware.ts
@@ -1,12 +1,12 @@
-import { Request, Response, NextFunction} from "express";
+import { Request, Response } from 'express';
 
 
-export default function errorHandler(error, req: Request, res: Response,next: NextFunction){
+export default function errorHandler(error, req: Request, res: Response){
     
-    console.log('Erroror:', error);
+	console.log('Erroror:', error);
 
-    if(error){
-        return res.status(error.type).send(error.message);
-    }
+	if(error){
+		return res.status(error.type).send(error.message);
+	}
 
 }
